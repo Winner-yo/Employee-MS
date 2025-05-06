d804fb4ec8dba4a024036b252667ec0e714a732b
Ashenafi Damena <ashenafidamena415@gmail.com>
2025-05-06 23:38:47 +0300

    dashboard and home

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import cors from "cors";
+import { adminRouter } from './Routes/adminRoutes.js';
 
 const app = express();
+app.use(
+  cors({
+      origin: "http://localhost:5173",
+      methods: ['GET', 'POST', 'PUT'],
+      credentials:true
+  })
+);
+app.use(express.json())
+app.use('/auth',adminRouter)
 
 app.listen(3000, () => {
     console.log('server start! ')
